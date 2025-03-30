9ddce6e39b5c87e9ffbeb119faad90d414172ad1
Rishvan <rishvan.m@globalgov.ae>
2025-03-30 22:01:17 +0400

    error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${KarlaSans.variable}  antialiased`}>
-        <StoreProvider children={children} />
+        <StoreProvider>{children}</StoreProvider>
       </body>
     </html>
   );
