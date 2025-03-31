05b9be793db11728ed745f63e25e15a0dc540f12
Rishvan <rishvan.m@globalgov.ae>
2025-03-31 22:43:48 +0400

    config udpate

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: process.env.NODE_ENV === "production" ? "/web" : "",
 };
 
 export default nextConfig;
