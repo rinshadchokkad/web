db46add2c66f3db7960b67badb0a39b8e0b49316
Rishvan <rishvan.m@globalgov.ae>
2025-03-31 22:49:13 +0400

    config updated

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: process.env.NODE_ENV === "development" ? "" : "/web",
+  // basePath: process.env.NODE_ENV === "development" ? "" : "/web",
+  basePath: "/web",
 };
 
 export default nextConfig;
